5f5035656806504788a4064763b8eec5d9b14ba0
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2020-04-11 11:07:14 +0200

    Initial commit.

---
+++ src/main/java/pl/jch/tests/kotlin/kotlinwebreactive/KotlinWebReactiveApplication.java
@@ -0,0 +1,13 @@
+package pl.jch.tests.kotlin.kotlinwebreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinWebReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotlinWebReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jch/tests/kotlin/kotlinwebreactive/KotlinWebReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jch.tests.kotlin.kotlinwebreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinWebReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
